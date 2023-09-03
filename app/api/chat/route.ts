6c73f353430ac28b2d57b0d1213897f313566f41
System Administrator <root@sanoyuusukenoMacBook-Pro.local>
2023-09-03 11:05:28 +0900

    入力フォーム

---
+++ app/api/chat/route.ts
@@ -0,0 +1,18 @@
+import { sleep } from "@/common/util";
+import { NextRequest } from "next/server";
+
+export async function POST(request:NextRequest) {
+    const { messageText } = await request.json()
+    const encoder = new TextEncoder()
+    const stream = new ReadableStream({
+        async start(controller) {
+            for(let i=0; i<messageText.length; i++) {
+                await sleep(100)
+                controller.enqueue(encoder.encode(messageText[i]))
+            }
+            controller.close()
+        }
+    })
+    return new Response(stream)
+    //使い方はMDNのStream APIを確認
+}
