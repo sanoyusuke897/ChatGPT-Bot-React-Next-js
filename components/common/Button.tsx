e5b6dc2da5b99f1fc9696bf8a7d13c3641086db8
System Administrator <root@sanoyuusukenoMacBook-Pro.local>
2023-09-08 14:51:54 +0900

    入力フォーム

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -7,22 +7,22 @@ type ButtonProps = {
 } & ComponentPropsWithoutRef<"button">
 
 export default function Button({
-    children, 
+    children,
     className = "",
     icon: Icon,
     variant = "default",
     ...props
 }: ButtonProps) {
     return (
-        <button 
-            className={`inline-flex items-center min-w-[38px] min-h-[38px] rounded px-3 py-1.5
-            ${ 
-                variant === "default" 
+        <button
+            className={`transition-colors inline-flex items-center min-w-[38px] min-h-[38px] rounded px-3 py-1.5
+            ${
+                variant === "default"
                     ? "text-black dark:text-gray-300 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-900"
-                    :variant === "outline"
+                    : variant === "outline"
                     ? "border border-gray-300 dark:border-gray-600 text-black dark:text-gray-300 bg-gray-50 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
-                    :variant === "primary" 
-                    ? "bg-primary-500 text-white hover:bg-primary-600 hover:text-white shadow-sm"
+                    : variant === "primary"
+                    ? "bg-primary-500 text-white hover:bg-primary-600 hover:text-white shadow-sm disabled:shadow-none disabled:bg-transparent disabled:text-gray-300 dark:disabled:text-gray-600"
                     : "text-black dark:text-gray-300 bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700"
             }
             ${className}`}
--- components/home/Main/ChatInput.tsx
+++ components/home/Main/ChatInput.tsx
@@ -1,21 +1,37 @@
 import Button from "@/components/common/Button"
 import { MdRefresh } from "react-icons/md"
-import { PiLightningFill } from "react-icons/pi"
+import { PiLightningFill, PiStopBold } from "react-icons/pi"
 import { FiSend } from "react-icons/fi"
 import TextareaAutoSize from "react-textarea-autosize"
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+import { useAppContext } from "@/components/AppContext"
+import { Message, MessageRequestBody } from "@/types/chat"
+import { ActionType } from "@/reducers/AppReducer"
 
 export default function ChatInput() {
     const [messageText, setMessageText] = useState("")
+    const {
+        state: { messageList, currentModel, streamingId }, dispatch
+    } = useAppContext()
 
     async function send() {
-        const body = JSON.stringify({ messageText })
+        const message: Message = {
+            id: uuidv4(),
+            role: "user",
+            content: messageText
+        }
+        const messages = messageList.concat([message])
+
+        const body: MessageRequestBody = { messages, model:currentModel }
+        dispatch({type: ActionType.ADD_MESSAGE, message })
+        setMessageText("")
         const response = await fetch("/api/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body
+            body: JSON.stringify(body)
         })
         if (!response.ok) {
             console.log(response.statusText)
@@ -25,28 +41,59 @@ export default function ChatInput() {
             console.log("body error")
             return
         }
+        const responseMessage: Message = {
+            id: uuidv4(),
+            role: "assistant",
+            content: ""
+        }
+        dispatch({type: ActionType.ADD_MESSAGE, message: responseMessage })
+        dispatch({
+            type: ActionType.UPDATE,
+            field: "streamingId",
+            value: responseMessage.id
+        })
         const reader = response.body.getReader()
         const decoder = new TextDecoder()
+        let content = ""
         let done = false
         while (!done) {
             const result = await reader.read()
             done = result.done
             const chunk = decoder.decode(result.value)
-            console.log(chunk)
+            content += chunk
+            dispatch({type: ActionType.UPDATE_MESSAGE, message: {...responseMessage, content} })
         }
+        dispatch({
+            type: ActionType.UPDATE,
+            field: "streamingId",
+            value: ""
+        })
         setMessageText("")
     }
 
     return (
         <div className='absolute bottom-0 inset-x-0 bg-gradient-to-b from-[rgba(255,255,255,0)] from-[13.94%] to-[#fff] to-[54.73%] pt-10 dark:from-[rgba(53,55,64,0)] dark:to-[#353740] dark:to-[58.85%]'>
             <div className='w-full max-w-4xl mx-auto flex flex-col items-center px-4 space-y-4'>
-                <Button
-                    icon={MdRefresh}
-                    variant='primary'
-                    className='font-medium'
-                >
-                    別の答えを試してみる
-                </Button>
+                {messageList.length !== 0 && (
+                    streamingId !== "" ? (
+                        <Button
+                        icon={PiStopBold}
+                        variant='primary'
+                        className='font-medium'
+                        >
+                            停止する
+                        </Button>
+                    ) : (
+                        <Button
+                        icon={MdRefresh}
+                        variant='primary'
+                        className='font-medium'
+                        >
+                            別の答えを試してみる
+                        </Button>
+                    )
+                    
+                )}
                 <div className='flex items-end w-full border border-black/10 dark:border-gray-800/50 bg-white dark:bg-gray-700 rounded-lg shadow-[0_0_15px_rgba(0,0,0,0.1)] py-4'>
                     <div className='mx-3 mb-2.5'>
                         <PiLightningFill />
@@ -63,6 +110,7 @@ export default function ChatInput() {
                     <Button
                         className='mx-3 !rounded-lg'
                         icon={FiSend}
+                        disabled = {messageText.trim() === "" || streamingId !== "" }
                         variant='primary'
                         onClick={send}
                     />
